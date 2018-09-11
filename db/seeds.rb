202e3610d7196b54c898c87f765c34a01cdff32c
hchu315 <chu.henry@hotmail.com>
2018-09-11 12:08:11 -0700

    Add cat model and index and show pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
+  cats = Cat.create([
+    {name: 'Fluffy', sex: 'F', color: 'grey', birth_date: '1/1/2016', description: 'The fluffiest cat around!' },
+    {name: 'Puma', sex: 'M', color: 'black', birth_date: '1/1/2016', description: 'The scariest cat around!' },
+    {name: 'Tom-Tom', sex: 'M', color: 'orange', birth_date: '3/9/2011', description: 'The oldest cat around!' },
+    {name: 'Steve', sex: 'F', color: 'calico', birth_date: '8/29/2018', description: 'The cohort cat!' }
+    ])
+end
